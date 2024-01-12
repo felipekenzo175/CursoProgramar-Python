7558549ca2b684c27770f45f6dcad0c1d11b8fff
felipekenzo175 <felipekenzoknz@gmail.com>
2024-01-12 00:06:52 -0300

    exercício bugado

---
+++ ExercicioBancoDeDadosModificado_05_11012024/main.py
@@ -0,0 +1,8 @@
+from ExercicioBancoDeDadosModificado_05_11012024 import *
+
+app = tk.Tk()
+
+objFrmExercicioBancoDeDadosModificado_05_11012024 = FrmExercicioBancoDeDadosModificado_05_11012024(app)
+objFrmExercicioBancoDeDadosModificado_05_11012024.pack()
+
+app.mainloop()
