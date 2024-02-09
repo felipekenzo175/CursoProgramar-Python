fed1b0c864f27f2b3d2e9ddce43d56f32c456740
felipekenzo175 <felipekenzoknz@gmail.com>
2024-02-08 22:22:54 -0300

    subindo excluir bd

---
+++ ExercicioExcluirBD_01_08022024/main.py
@@ -0,0 +1,8 @@
+from ExercicioExcluirBD_01_08022024 import *
+
+app = tk.Tk()
+
+objFrmExercicioExcluirBD_01_08022024 = FrmExercicioExcluirBD_01_08022024(app)
+objFrmExercicioExcluirBD_01_08022024.pack()
+
+app.mainloop()
